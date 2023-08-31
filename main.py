080efad1c13d42a722dc8a045c980a39fee5a06f
Federico Betti <federico.betti@outlook.com>
2023-08-31 08:47:07 +0200

    main update

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ from pytorch_lightning.loggers import CSVLogger
 from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer
 import torch
 from pytorch_lightning import LightningModule, Trainer
-from pytorch_lightning.callbacks import EarlyStopping
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.metrics import accuracy_score
 from torch import nn
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
+import numpy as np
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-# device = "cpu"
+device = "cpu"
+batch_size = 64
 # Initialize an empty dictionary
 imagenet_labels_to_classes = {}
+
+# fix all seeds for reproducibility
+torch.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(0)
 
 # Read the file and populate the dictionary
 with open("imagenet_class_labels.txt", "r") as f:
@@ -25,7 +33,7 @@ with open("imagenet_class_labels.txt", "r") as f:
         value = elements[1]
         imagenet_labels_to_classes[key] = value.strip()
 
-BASIC_STRING = "An image containing one of the following: "
+BASIC_STRING = ""
 
 
 class CustomImageFolder(ImageFolder):
@@ -45,6 +53,7 @@ class CustomImageFolder(ImageFolder):
                 )
             else:
                 # Use the original class name
+                class_name = class_name.split(".")[-1].lower()
                 self.class_text_labels.append(class_name)
         self.text_features = None
         self.texts = None
@@ -98,8 +107,8 @@ val_size = len(train_dataset) - train_size
 calltech_train, calltech_val = random_split(
     train_dataset, [train_size, val_size]
 )
-train_loader = DataLoader(calltech_train, batch_size=32, shuffle=True)
-val_loader = DataLoader(calltech_val, batch_size=32)
+train_loader = DataLoader(calltech_train, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(calltech_val, batch_size=batch_size)
 
 
 class FineTuneCLIP(LightningModule):
@@ -112,8 +121,6 @@ class FineTuneCLIP(LightningModule):
         self.tokenizer = CLIPTokenizer.from_pretrained(
             "openai/clip-vit-base-patch32"
         )
-        self.loss_img = nn.CrossEntropyLoss()
-        self.loss_txt = nn.CrossEntropyLoss()
 
     def get_train_dataset(self):
         return self.trainer.train_dataloader.dataset.dataset
@@ -139,6 +146,9 @@ class FineTuneCLIP(LightningModule):
     def forward(self, images, texts):
         return self.model(images, texts)
 
+    def on_train_epoch_start(self) -> None:
+        self.model.train()
+
     def training_step(self, batch, batch_idx):
         images, labels = batch
         images, labels = images.to(self.device), labels.to(self.device)
@@ -157,7 +167,6 @@ class FineTuneCLIP(LightningModule):
             prog_bar=True,
             logger=True,
         )
-
         return {"loss": loss}
 
     def validation_step(self, batch, batch_idx):
@@ -228,17 +237,12 @@ class FineTuneCLIP(LightningModule):
             prog_bar=True,
             logger=True,
         )
-        print("Predictions: ", predictions)
-        print("Test Accuracy: ", test_acc)
         return {"test_acc": test_acc}
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
-            self.parameters(),
-            lr=5e-5,
-            betas=(0.9, 0.98),
-            eps=1e-6,
-            weight_decay=0.2,
+            self.model.parameters(),
+            lr=5e-3,
         )
         return optimizer
 
@@ -248,19 +252,31 @@ model = FineTuneCLIP()
 
 # Early stopping based on validation loss
 early_stop_callback = EarlyStopping(
-    monitor="val_loss", min_delta=0.00, patience=3, verbose=True, mode="min"
+    monitor="val_loss", min_delta=0.0, patience=3, verbose=True, mode="min"
 )
-csv_logger = CSVLogger("logs", name=f"fine-tune-clip_{datetime.now()}")
+current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+csv_logger = CSVLogger("logs", name=f"fine-tune-clip_{current_time}")
+# Initialize the Model Checkpoint callback
+checkpoint_callback = ModelCheckpoint(
+    monitor="val_loss",  # Replace with your validation metric
+    mode="min",  # Mode can be 'max' or 'min' depending on the metric
+    save_top_k=1,  # Save only the best model
+    filename="{epoch}-{val_loss:.2f}",  # Filename format
+    verbose=True,  # Log when a better model is found
+)
 
 # Trainer setup
 trainer = Trainer(
     max_epochs=30,
-    callbacks=[early_stop_callback],
+    callbacks=[early_stop_callback, checkpoint_callback],
     accelerator=device,
     enable_progress_bar=True,
     num_sanity_val_steps=0,
     logger=csv_logger,
+    log_every_n_steps=10,
 )
+
+trainer.test(model, dataloaders=[test_dataloader])
 
 trainer.fit(model, train_loader, val_loader)
 
